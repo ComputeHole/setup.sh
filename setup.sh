b76019d30922702e72e21472923d0c463dbbfb70
mwoolweaver <m.woolweaver@yahoo.com>
2019-05-06 05:54:07 -0500

    check depends before scripts run
    and setup
    
    sudo wget -O - https://swupdate.openvpn.net/repos/repo-public.gpg|apt-key add -
    sudo echo "deb http://build.openvpn.net/debian/openvpn/release/2.4 bionic main" > /etc/apt/sources.list.d/openvpn-aptrepo.list
    sudo add-apt-repository ppa:ondrej/apache2 -y
    sudo add-apt-repository ppa:ondrej/php -y
    sudo wget https://dev.mysql.com/get/mysql-apt-config_0.8.13-1_all.deb
    sudo dpkg -i mysql-apt-config_0.8.13-1_all.deb

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC1090
 
-echo "changing to ~setup.sh"
+echo "making directory ~/setup.sh"
 mkdir ~/setup.sh
+echo "change to ~/setup.sh"
 cd ~/setup.sh
 echo "Download https://install.pi-hole.net"
 wget https://install.pi-hole.net -O install.pi-hole.net
@@ -11,6 +12,14 @@ wget https://install.pivpn.io -O install.pivpn.io
 echo "making scripts executable"
 sudo chmod +x install.pi-hole.net
 sudo chmod +x install.pivpn.io
+
+echo "setup repos"
+sudo wget -O - https://swupdate.openvpn.net/repos/repo-public.gpg|apt-key add -
+sudo echo "deb http://build.openvpn.net/debian/openvpn/release/2.4 bionic main" > /etc/apt/sources.list.d/openvpn-aptrepo.list
+sudo add-apt-repository ppa:ondrej/apache2 -y
+sudo add-apt-repository ppa:ondrej/php -y
+sudo wget https://dev.mysql.com/get/mysql-apt-config_0.8.13-1_all.deb
+sudo dpkg -i mysql-apt-config_0.8.13-1_all.deb
 
 sleep 5s
 
@@ -21,6 +30,16 @@ sleep 5s
 
 echo "Upgrading available packages"
 sudo apt upgrade
+
+sleep 5s
+
+echo "installing and confirgure dependencies"
+sudo apt install apt-utils dialog debconf dhcpcd5 git cron curl dnsutils iputils-ping lsof netcat psmisc unzip wget idn2 sqlite3 libcap2-bin dns-root-data resolvconf libcap2 iproute2 php7.2-common php7.2-cgi php7.2-sqlite3 apache2 libapache2-mod-php7.2 openvpn iptables-persistent dnsutils expect whiptail net-tools
+
+sleep 1s
+
+sudo a2enmod php7.2
+sudo service apache2 restart
 
 sleep 5s
 
